5ff36559c51647d201488b4f1a7952f813e03d2c
Andres Concha <Andres Concha>
2019-04-28 07:06:45 -0500

    Pagina retos finalizado

--- src/components/Challenges.vue
+++ src/components/Challenges.vue
@@ -13,42 +13,49 @@
           >¡Tú eres el actor principal! Ahora nos ayudarás en la elección de los 5 desafíos de cara a los Objetivos del Desarrollo Sostenible que nuestra ciudad debe afrontar al tiempo que se fomenta el involucramiento ciudadano en el proceso electoral local. Junto a ti, encontraremos las soluciones más innovadoras para hacer frente a estos desafíos. Los Objetivos del Desarrollo Sostenible conllevan un espíritu de colaboración y pragmatismo para elegir las mejores opciones con el fin de mejorar la vida de manera sostenible para las generaciones futuras. Se abordan las causas sociales fundamentales y nos unen para para emprender un nuevo camino en el que se focalice el mejoramiento de la vida de todos, sin dejar a nadie atrás.</p>
         </div>
       </div>
+      <!-- INICIO DE CHALLENGES -->
       <div class="row second justify-content-around">
-        <router-link tag="div" to="/reto" class="challenge container-fluid">
+
+        <router-link tag="div" to="/reto" class="challenge container-fluid" v-for="(challenge, index) in challenges" :key="index">
           <div class="row image">
-            <img
-              src="https://ep01.epimg.net/internacional/imagenes/2018/07/23/billete_a_macondo/1532310440_143390_1532310884_noticia_normal.jpg"
-              alt="Imagen de reto"
-            >
+            <img v-bind:src="challenge.picture.url" alt="Imagen de reto">
           </div>
           <div class="row data">
-            <h3 class="title">El titulo del reto</h3>
-            <p class="parag">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+            <h3 class="title">{{challenge.title}}</h3>
+            <p class="parag">{{challenge.description}}</p>
           </div>
         </router-link>
-        <router-link tag="div" to="/reto" class="challenge container-fluid">
-          <div class="row image">
-            <img
-              src="https://ep01.epimg.net/internacional/imagenes/2018/07/23/billete_a_macondo/1532310440_143390_1532310884_noticia_normal.jpg"
-              alt="Imagen de reto"
-            >
-          </div>
-          <div class="row data">
-            <h3 class="title">El titulo del reto</h3>
-            <p class="parag">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
-          </div>
-        </router-link>
+
       </div>
     </div>
   </div>
 </template>
 <script>
+import {SERVER_URL} from '../variables.js'
+
 import Header from "./Header.vue";
 import Idea from "./addIdea.vue";
+
 export default {
   components: {
 		Header,
 		Idea
+  },
+  data(){
+    return{
+      challenges: []
+    }
+  },
+  methods: {
+    loadChallenges(){
+        this.$http.get(SERVER_URL + '/challenges').then(function(response){
+            this.challenges = response.data;
+            // this.challenges = response.data.slice().reverse();
+        })
+    },
+  },
+  created(){
+    this.loadChallenges();
   }
 };
 </script>
