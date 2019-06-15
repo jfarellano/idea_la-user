f36df30dab4bfcf694266501fe2eef591e6494b0
Juan Arellano <jfarellano@uninorte.edu.co>
2019-06-15 11:39:35 -0500

    Remove commented styles

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,7 @@ export default {
     font-family: 'Rubik', sans-serif;
   }
   #app {
-    /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    /* text-align: justify; */
-    /* color: #2c3e50; */
-    /* margin-top: 60px; */
   }
 </style>
