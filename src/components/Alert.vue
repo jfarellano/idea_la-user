48436613f4c3d0a8157c26be6b42aa89b3e63688
Juan Arellano <jfarellano@uninorte.edu.co>
2019-06-15 11:38:31 -0500

    Se agrega componente de alertas

---
+++ src/components/Alert.vue
@@ -0,0 +1,47 @@
+<template>
+  <vue-snotify></vue-snotify>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			params: {
+        timeout: 2000,
+        showProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true
+      }
+		}
+	},
+  methods: {
+    error: function(title, message) {
+      this.$snotify.error(message, title, this.params);
+		},
+		simple: function(title, message) {
+      this.$snotify.simple(message, title, this.params);
+		},
+		success: function(title, message) {
+      this.$snotify.success(message, title, this.params);
+		},
+		warning: function(title, message) {
+      this.$snotify.warning(message, title, this.params);
+		},
+		confirm: function(title, message, accept, deny) {
+      this.$snotify.confirm(message, title, {
+        timeout: 2000,
+        showProgressBar: false,
+        closeOnClick: true,
+				pauseOnHover: true,
+				buttons: [
+					{text: 'Aceptar', action: () => accept()},
+					{text: 'Cancelar', action: () => deny()}
+				]
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
